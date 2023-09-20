f6ac79c786c0ab224e1dbf75d9f76f85b40a9571
Florent Poinsaut <florent@poinsaut.fr>
2023-09-20 10:52:02 +0200

    Refacto

--- bin/merge-tuxgasy-work.sh
+++ bin/merge-tuxgasy-work.sh
@@ -1,4 +1,21 @@
 #!/usr/bin/env sh
+#
+# Copyright (C) 2022-2023 Solution Libre <contact@solution-libre.fr>
+# 
+# This file is part of Docker Dolibarr.
+# 
+# Docker Dolibarr is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+# 
+# Docker Dolibarr is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+# 
+# You should have received a copy of the GNU General Public License
+# along with Docker Dolibarr.  If not, see <http://www.gnu.org/licenses/>.
 
 CWD=$(pwd)
 DIR='tuxgasy-dolibarr'
